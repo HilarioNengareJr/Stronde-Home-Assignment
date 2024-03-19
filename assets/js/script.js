704b30f86a149ebab22310ba1aba7a9503140d3f
Hilario Nengare <hnengare@gmail.com>
2024-03-19 03:16:35 +0300

    fixed commat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
 'use strict';
 
+
+/**
+ * common variables
+ */
+
+const navbar = document.querySelector('[data-navbar]');
+const navToggleBtns = document.querySelectorAll('[data-nav-toggle-btn]');
+const navLinks = document.querySelectorAll('[data-nav-link]');
+const expandBtn = document.querySelector('[data-expand-btn]');
+const heroList = document.querySelector('[data-hero-list]');
+const brandSpan = document.querySelector('[data-brand]');
+
+
 /**
  * navbar toggle
  */
 
 
-const navbar = document.querySelector('[data-navbar');
-const navToggleBtn = document.querySector('[data-nav-toggle-btn]');
-const navLinks = document.querySelectorAll('[data-nav-link]');
+for (let i = 0; i < navToggleBtns.length; i++) {
+    navToggleBtns[i].addEventListener("click", function () {
+        navbar.classList.toggle("active");
+        this.classList.toggle("active");
+        expandBtn.classList.toggle("active");
+    });
+}
 
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function () {
+        navbar.classList.remove("active");
+        this.classList.remove("active");
+        expandBtn.classList.remove("active");
+    })
+}
 
 
 /**
@@ -17,13 +41,33 @@ const navLinks = document.querySelectorAll('[data-nav-link]');
 
 const header = document.querySelector('[data-header]');
 
-
-window.addEventListener("scroll",function (){
-    if (this.window.scrollY >= 40) {
+window.addEventListener("scroll", function () {
+    if (this.window.scrollY >= 50) {
         header.classList.add('active');
     } else {
         header.classList.remove('active');
     }
-} )
+});
 
 
+/**
+ * master template
+ */
+
+const brands = ["Coca Cola Brand", "Unilever Brand", "Waterworld Waterpark Ayla Napa", "Amazon electronics"];
+
+brands.forEach(brand => {
+    heroList.innerHTML += `
+            <li>
+            <div class="brand-card">
+            <a href="#" target="_blank">
+                <span class="brand" data-brand>${brand}</span>
+
+                <span class="ion-icon">
+                <ion-icon name="chevron-forward-outline"></ion-icon>
+                </span>
+            </a>
+            </div>
+        </li>
+        `
+});
